5a4d7c9a90c3c5dcc8c6135551f09f18aaeaaf44
bmacheski <bwm0007@gmail.com>
2016-03-31 13:44:23 -0500

    Move files

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,97 +1,93 @@
-let username = 'bmacheski';
-let defaultUrl = `https://api.github.com/users/${username}/starred`;
-let newUrl;
-let numPages;
+$(() => {
+  let username = 'bmacheski';
+  let defaultUrl = `https://api.github.com/users/${username}/starred`;
+  let newUrl;
+  let numPages;
 
-function parseHeader(linkStr) {
-  let url;
+  function parseHeader(linkStr) {
+    let url;
 
-  if (linkStr) {
-    let s = linkStr.split(',').map(function(rel) {
+    if (linkStr) {
+      let s = linkStr.split(',').map(rel => {
+        return rel.split(';').map((item, idx) => {
+          let hold = item.split(',');
 
-      return rel.split(';').map(function(item, idx) {
-        let hold = item.split(',');
-
-        if (idx === 0) {
-          if (!url) {
-            url = hold[0].split('?page=')[0].replace('<', '').trim();
+          if (idx === 0) {
+            if (!url) {
+              url = hold[0].split('?page=')[0].replace('<', '').trim();
+            }
+            return hold[0].split('?page=')[1].replace('>', '');
           }
-          return hold[0].split('?page=')[1].replace('>', '');
-        }
-        if (idx === 1) {
-          return hold[0].split('rel="')[1].replace(/\"/g, '');
-        }
-      })
-    }).reduce(function(res, curr) {
-      res[curr[1]] = curr[0];
+          if (idx === 1) {
+            return hold[0].split('rel="')[1].replace(/\"/g, '');
+          }
+        })
+      }).reduce((res, curr) => {
+        res[curr[1]] = curr[0];
+        return res;
+      }, {});
 
-      return res;
-    }, {})
-
-    return { url: url, o: s };
+      return {
+        url : url
+        , o : s
+      }
+    }
   }
-}
 
-function ghRequest(cb, num) {
-  let u = newUrl ? `${newUrl}?page=${num}` : defaultUrl;
+  function ghRequest(cb, num) {
+    let ghUrl = newUrl ? `${newUrl}?page=${num}` : defaultUrl;
 
-  $.ajax({
-    type: 'GET',
-    url: u
-  })
-  .done(function(data, status, xhr) {
-    let link = xhr.getResponseHeader('Link');
-    let res = parseHeader(link);
+    $.ajax({
+      type  : 'GET'
+      , url : ghUrl
+    })
+    .done((data, status, xhr) => {
+      let link = xhr.getResponseHeader('Link')
+      , res    = parseHeader(link);
 
-    if (res && res.o) {
-      numPages = res.o.last;
-      newUrl = res.url;
-    }
+      if (res && res.o) {
+        numPages = res.o.last
+        newUrl   = res.url
+      }
+      if (typeof(cb) === 'function') {
+        cb(data);
+      }
+      if (typeof(num) === 'function') {
+        num();
+      }
+    });
+  }
 
-    if (typeof(cb) === 'function') {
-      cb(data);
-    }
+  function paginate() {
+    $('#pagination').pagination({
+      itemsOnPage   : 30
+      , pages       : numPages
+      , cssStyle    : 'light-theme'
+      , onPageClick : (num) => {
+          let bookmarkList = $('#bookmarks');
+          ghRequest(parseBookmarks, num);
+        }
+    });
+  }
 
-    if (typeof(num) === 'function') {
-      num();
-    }
-  });
-}
+  function parseBookmarks(json) {
+    let bookmarkList = $('#bookmarks');
+    bookmarkList.empty();
 
-function paginate() {
-  $('#pagination').pagination({
-    itemsOnPage: 30,
-    pages: numPages,
-    cssStyle: 'light-theme',
-    onPageClick: function(num) {
-      let bookmarkList = $('#bookmarks');
-
-      ghRequest(parseBookmarks, num);
-    }
-  });
-}
-
-function parseBookmarks(json) {
-  let bookmarkList = $('#bookmarks');
-
-  bookmarkList.empty();
-
-  $.each(json, (i, el) => {
-    bookmarkList.append(
-      `<li class='title'>
-        <a href="${json[i].html_url}">${json[i].name}</a>
-        <p>${json[i].description}</p>
-      </li>`
-    );
-  });
-};
-
-$(document).ready(() => {
-  ghRequest(parseBookmarks, paginate);
+    $.each(json, (i, el) => {
+      bookmarkList.append(
+        `<li class='title'>
+          <a href="${json[i].html_url}">${json[i].name}</a>
+          <p>${json[i].description}</p>
+        </li>`
+      )
+    });
+  }
 
   $('body').on('click', 'a', function() {
     chrome.tabs.create({ url: $(this).attr('href') });
-
     return false;
   });
+
+  ghRequest(parseBookmarks, paginate);
 });
